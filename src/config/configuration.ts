5fa39ae65d7525b3660f2ce0ee9045cce0da7bac
Nut Pinyo <bomb.NP@gmail.com>
2021-01-29 13:18:18 +0700

    Update src/config/configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,4 @@
 export default () => ({
-	mongoURI: process.env.MONGO_URI || 'http://localhost:27017/cugetreg',
+	mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/cugetreg',
 	port: parseInt(process.env.PORT, 10) || 3000,
 })
