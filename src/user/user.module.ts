6715a7f266e1662a35f5c6825645fb71d9cd9219
Nut Pinyo <bomb.np@gmail.com>
2021-01-30 17:32:48 +0700

    :card_file_box: add user schema

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserResolver } from './user.resolver'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UserSchema } from 'src/schemas/user.schema'
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
 	providers: [UserResolver, UserService],
 })
 export class UserModule {}
