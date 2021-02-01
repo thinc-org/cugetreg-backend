807c01c5461f45e59ca1b939694aee6360690967
Nut Pinyo <bomb.np@gmail.com>
2021-02-01 20:05:32 +0700

    add auth guard, strategy, and accessToken dto

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { AccessTokenDTO } from './auth.dto'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwtSecret'),
+    })
+  }
+
+  validate(payload: AccessTokenDTO): AccessTokenDTO {
+    return { _id: payload._id, firstName: payload.firstName }
+  }
+}
