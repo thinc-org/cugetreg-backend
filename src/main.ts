356a6a374eda1cfd885a9f6a51c5d71452506538
Nut Pinyo <bomb.np@gmail.com>
2021-02-02 12:33:38 +0700

    remove cookies

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
-import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
-
-  app.use(cookieParser())
 
   const port = configService.get<number>('port')
   await app.listen(port, () => {
