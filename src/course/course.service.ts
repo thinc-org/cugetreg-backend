4338231abf36b74c4de79cbcf96d4a20574a73b7
Nut Pinyo <bomb.np@gmail.com>
2021-02-02 22:11:45 +0700

    added initial course fetching

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
+import { Course, getMockCourses } from '@thinc-org/chula-courses'
 
 @Injectable()
-export class CourseService {
-  findAll() {
-    return `This action returns all course`
+export class CourseService implements OnApplicationBootstrap {
+  private courses: Course[] = []
+
+  async onApplicationBootstrap(): Promise<void> {
+    console.log('Started fetching course data...')
+    this.courses = await getMockCourses()
+    console.log('Finished fetching course data...')
+  }
+
+  findAll(): Course[] {
+    return this.courses
   }
 
   findOne(id: number) {
