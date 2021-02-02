e1d1fe750e86738c5d88a2eca6f731d52bbda7aa
Nut Pinyo <bomb.np@gmail.com>
2021-02-02 22:25:44 +0700

    add course refresh endpoint

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CourseService } from './course.service'
 import { CourseResolver } from './course.resolver'
+import { CourseController } from './course.controller'
 
 @Module({
   providers: [CourseResolver, CourseService],
+  controllers: [CourseController],
 })
 export class CourseModule {}
