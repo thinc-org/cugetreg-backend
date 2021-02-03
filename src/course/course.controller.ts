b11d00e2263a1892a18a136e117b24e07242d82a
Nut Pinyo <bomb.np@gmail.com>
2021-02-03 20:40:17 +0700

    protect refresh route

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Post } from '@nestjs/common'
+import { BadRequestException, Controller, Post, Query } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { CourseService } from './course.service'
 
 @Controller('course')
 export class CourseController {
-  constructor(private courseService: CourseService) {}
+  constructor(
+    private courseService: CourseService,
+    private configService: ConfigService
+  ) {}
 
   @Post('refresh')
-  async refresh() {
+  async refresh(@Query('secret') secret: string): Promise<void> {
+    if (typeof secret === 'undefined') {
+      throw new BadRequestException({
+        reason: 'SECRET_UNDEFINED',
+        message: 'Refresh secret is undefined.',
+      })
+    }
+    if (secret !== this.configService.get<string>('refreshSecret')) {
+      throw new BadRequestException({
+        reason: 'SECRET_INVALID',
+        message: 'Refresh secret is incorrect.',
+      })
+    }
     this.courseService.refresh()
   }
 }
