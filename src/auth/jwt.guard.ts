8ae5bf39712201eb7fdbd1e38e18fa2d0609bd7d
Nut Pinyo <bomb.np@gmail.com>
2021-02-07 23:03:35 +0700

    Revert "add allowUnauthorized decorator for queries that optionally use userId"
    This reverts commit f40810628fc75b4f9388a24429b7b9ebb9332a23.

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,29 +1,12 @@
 import { ExecutionContext, Injectable } from '@nestjs/common'
-import { Reflector } from '@nestjs/core'
 import { GqlExecutionContext } from '@nestjs/graphql'
 import { AuthGuard } from '@nestjs/passport'
-import { ALLOW_UNAUTHORIZED_KEY } from 'src/common/decorators/allowUnauthorized.decorator'
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
-    super()
-  }
-
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context)
     return ctx.getContext().req
-  }
-
-  canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(
-      ALLOW_UNAUTHORIZED_KEY,
-      [context.getHandler(), context.getClass()]
-    )
-    if (isPublic) {
-      return true
-    }
-    return super.canActivate(context)
   }
 }
 
