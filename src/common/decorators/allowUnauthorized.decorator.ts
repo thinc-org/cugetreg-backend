0fe4caf7f13035ffc72a0f2a26e7dba341bb7b9a
Nut Pinyo <bomb.np@gmail.com>
2021-02-07 23:03:35 +0700

    Revert "add allowUnauthorized decorator for queries that optionally use userId"
    This reverts commit f40810628fc75b4f9388a24429b7b9ebb9332a23.

--- src/common/decorators/allowUnauthorized.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common'
-
-export const ALLOW_UNAUTHORIZED_KEY = 'allowUnauthorized'
-export const AllowUnauthorized = () => SetMetadata(ALLOW_UNAUTHORIZED_KEY, true)
