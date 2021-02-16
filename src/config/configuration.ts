4a5e20111b7f7206a1f417d571f2fa96f4b1ae79
Nut Pinyo <bomb.np@gmail.com>
2021-02-16 18:03:21 +0700

    validate required configs on startup

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,6 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
 export default () => ({
   mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/cugetreg',
   port: parseInt(process.env.PORT, 10) || 3000,
@@ -7,3 +10,18 @@ export default () => ({
   refreshSecret: process.env.REFRESH_SECRET,
   isProduction: process.env.IS_PRODUCTION === 'true',
 })
+
+export function validateConfig(configService: ConfigService): void {
+  const logger = new Logger('ConfigService')
+  const config = {
+    googleOAuthId: configService.get<string>('googleOAuthId'),
+    googleOAuthSecret: configService.get<string>('googleOAuthSecret'),
+    jwtSecret: configService.get<string>('jwtSecret'),
+    refreshSecret: configService.get<string>('refreshSecret'),
+  }
+  for (const key in config) {
+    if (!config[key]) {
+      logger.error(`Config "${key}" is undefined.`)
+    }
+  }
+}
