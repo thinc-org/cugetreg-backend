dfabd72fb00927cac1ee20d568165738f088708f
Nut Pinyo <bomb.np@gmail.com>
2021-02-16 17:27:35 +0700

    refresh access token if access token expires

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,13 @@ import { UserService } from './user.service'
 import { UserResolver } from './user.resolver'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UserSchema } from 'src/schemas/user.schema'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
   providers: [UserResolver, UserService],
 })
 export class UserModule {}
