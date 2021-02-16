42b9257cb4ab9dbbeb2a0af1dc9a769d1889768d
Nut Pinyo <bomb.np@gmail.com>
2021-02-16 18:03:21 +0700

    validate required configs on startup

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { HttpExceptionFilter } from './common/filters/httpException.filter'
+import { validateConfig } from './config/configuration'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+
+  validateConfig(configService)
 
   const port = configService.get<number>('port')
   const isProduction = configService.get<boolean>('isProduction')
