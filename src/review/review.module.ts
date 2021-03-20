e4dc665fa4f0dbf0eb2bb5419512a789adfdc5e2
Nut Pinyo <bomb.np@gmail.com>
2021-03-20 13:36:50 +0700

    add airtable api key env and airtable client

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common'
+import { HttpModule, Module } from '@nestjs/common'
 import { ReviewService } from './review.service'
 import { ReviewResolver } from './review.resolver'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ReviewSchema } from 'src/schemas/review.schema'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'review', schema: ReviewSchema }]),
+    HttpModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        baseURL: 'https://api.airtable.com/v0/appDUOJ3KIOgYiYZy/Reviews',
+        headers: {
+          Authorization: `Bearer ${configService.get<string>(
+            'airtableApiKey'
+          )}`,
+        },
+      }),
+      inject: [ConfigModule],
+    }),
   ],
   providers: [ReviewResolver, ReviewService],
   exports: [ReviewService],
